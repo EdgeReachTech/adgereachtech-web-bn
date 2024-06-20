0acfe1901ee121bf625b89d5cd29b5c4af748e3b
RWIGARA Rodrigue <rodrirwigara@gmail.com>
2024-06-20 17:27:58 +0200

    refactor user model

--- src/models/User.ts
+++ src/models/User.ts
@@ -3,11 +3,11 @@ import { Document, model, Schema } from 'mongoose'
 interface interUser extends Document {
      firstName:string
      lastName:string
-      location:string
-      dateOfBirth:Date
+     location:string
+     dateOfBirth:Date
      status:string
      role:string
-     gander:string
+     gender:string
      isverified:boolean
      verifiedAt:Date
      password: string;
@@ -19,14 +19,14 @@ interface interUser extends Document {
      firstName:{type:String, required:true },
      lastName:{type:String, required:true },
      location:{type:String, required:true,  default:'Unknown'},
-     dateOfBirth:{type:Date, required:true,  default:null},
-     status:{type:String, required:true, default:'unverified'},
-     role:{type:String, required:true, default:'developer'},
-     gander:{type:String, required:true , default:'Unknown' },
+     dateOfBirth:{type:Date, required:false},
+     status:{type:String, required:true,enum:['pending','active','blocked'], default:'pending'},
+     role:{type:String, required:true,enum:['developer','President','Project Manager','Quality Assurence','Vice President'], default:'developer'},
+     gender:{type:String, required:true,enum:['male','female','other'] , default:'other'},
      isverified:{type:Boolean, required:true , default:false},
      verifiedAt:{type:Date, required:false},
      password: {type:String, required:true },
-    createdAt:{type:Date, required:true, default:Date.now()}
+     createdAt:{type:Date, required:true, default:Date.now()}
     })
  
     const User = model<interUser>('User', userSchema)
