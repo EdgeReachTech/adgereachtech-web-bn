26baa15033ace7ead2861ffbe5f5ae36c1f239ad
Your Name <fiacrepcc@gmail.com>
2024-06-24 02:25:37 +0200

    MADE  NEW BRANCH CALLED VALIDATE-FEATURE and i wrote code to validate user data and is working properly

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { userController } from "../controllers/userControllers";
+import { validateUser } from "../validations/userValidation";
 
 export const userRouter = express.Router()
 
-userRouter.post('/register', userController.registerUser)
+userRouter.post('/register', validateUser, userController.registerUser);
---
+++ src/validations/userValidation.ts
@@ -0,0 +1,41 @@
+
+// codes to validate user written by fiacre
+import { check, validationResult } from 'express-validator';
+import { Request, Response, NextFunction } from 'express';
+
+export const validateUser = [
+  check('firstName')
+    .trim()
+    .notEmpty().withMessage('First name is required')
+    .isAlpha().withMessage('First name must only contain letters'),
+
+  check('lastName')
+    .trim()
+    .notEmpty().withMessage('Last name is required')
+    .isAlpha().withMessage('Last name must only contain letters'),
+
+  check('email')
+    .isEmail().withMessage('Email is invalid'),
+
+  check('location')
+    .notEmpty().withMessage('Location is required'),
+
+  check('dateOfBirth')
+    .optional()
+    .isISO8601().withMessage('Date of birth must be a valid date, eg 2000-03-31'),
+
+  check('gender')
+    .optional()
+    .isIn(['male', 'female']).withMessage('Gender must be either Male or Female'),
+
+  check('password')
+    .isStrongPassword().withMessage('Password must be strong, containing special char, Upper case letter and at least 8 words'),
+
+    (req: Request, res: Response, next: NextFunction) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+          return res.status(400).json({ errors: errors.array() });
+        }
+        next();
+      }
+    ];
