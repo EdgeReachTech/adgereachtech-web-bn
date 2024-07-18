775b0780f56a87955ec8570f09b3195de7b3c091
happycodez <happybrown2k@gmail.com>
2024-07-18 21:02:03 +0200

    isLoggedIn added

--- src/routes/messageRoutes.ts
+++ src/routes/messageRoutes.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import { messageController } from '../controllers/messageControllers';
+import { isLoggedIn } from '../middleware/authentication';
 
 export const messageRouter = express.Router();
 
-messageRouter.post("/createMessage/:receiverId", messageController.createMessage);
-messageRouter.patch("/updateMessage/:messageId", messageController.updateMessage);
-messageRouter.patch("readMessage/:messageId", messageController.readMessage);
-messageRouter.delete("/deleteMessage/:messageId", messageController.deleteMessage);
+messageRouter.post("/createMessage/:receiverId", isLoggedIn, messageController.createMessage);
+messageRouter.patch("/updateMessage/:messageId", isLoggedIn, messageController.updateMessage);
+messageRouter.patch("readMessage/:messageId", isLoggedIn, messageController.readMessage);
+messageRouter.delete("/deleteMessage/:messageId", isLoggedIn, messageController.deleteMessage);
