6e62d6863ff63762523f9b310006df3ce29e83ed
RWIGARA Rodrigue <rodrirwigara@gmail.com>
2025-02-02 23:48:27 +0200

    rm new dependencies

--- src/config/cloudinary.ts
+++ src/config/cloudinary.ts
@@ -1,12 +1,12 @@
-import { v2 as cloudinary } from 'cloudinary';
-import dotenv from 'dotenv';
+// import { v2 as cloudinary } from 'cloudinary';
+// import dotenv from 'dotenv';
 
-dotenv.config();
+// dotenv.config();
 
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_NAME || '',
-  api_key: process.env.CLOUDINARY_KEY||'',
-  api_secret: process.env.CLOUDINARY_SECRET||"",
-});
+// cloudinary.config({
+//   cloud_name: process.env.CLOUDINARY_NAME || '',
+//   api_key: process.env.CLOUDINARY_KEY||'',
+//   api_secret: process.env.CLOUDINARY_SECRET||"",
+// });
 
-export default cloudinary;
+// export default cloudinary;
